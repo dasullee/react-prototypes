ab102aba7efbecc241c315e045e6f658420ebc1d
Dasul Lee <nicoledasullee@gmail.com>
2019-06-14 23:23:27 -0700

    Set up first greeting page with lucky number

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const luckyNumber = () => Math.floor(Math.random() * 999 + 1)
+
+const greeting = (namdAndNumber) => {
+    const container = <div className="container">
+        <h1>Hello, {namdAndNumber.name}! Nice to meet you!</h1>
+        <h2 className="text-muted">Your number is: {namdAndNumber.luckyNumber}</h2>
+    </div>
+    return container
+}
+const user = {
+    name: "Nicole",
+    luckyNumber: luckyNumber()
+}
+
 ReactDOM.render(
-    <h1 className="container text-center">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
